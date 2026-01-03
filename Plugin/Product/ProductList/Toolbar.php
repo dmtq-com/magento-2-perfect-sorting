f05cd5a90dd4dd7484580930edcc152eacce6bbc
Clover <clover.zhou@hotmail.com>
2026-01-03 09:35:50 +0800

    Refactor Toolbar class to handle sorting logic

--- Plugin/Product/ProductList/Toolbar.php
+++ Plugin/Product/ProductList/Toolbar.php
@@ -27,8 +27,25 @@ class Toolbar
 
     public function aroundSetCollection(ProductData $subject, \Closure $proceed, $collection)
     {
-        if ($this->_request->getParam("product_list_order") === Config::NEWEST_SORTING_SORT_KEY) {
+        $requestOrder = $this->_request->getParam("product_list_order");
+        $catalogPage = $subject->getRequest()->getModuleName() == 'catalog';
+
+        if (in_array($requestOrder, [Config::NEWEST_SORTING_SORT_KEY, Config::BEST_SELLERS_SORTING_SORT_KEY])) {
             $subject->setDefaultDirection('desc');
+        }
+
+        //$catId = (int)$this->_request->getParam("id");
+        $catPath = $subject->getRequest()->getUri()->getPath();
+
+        if (!$requestOrder && $catalogPage) {
+            if ($catPath == '/new-arrivals.html') {
+                $subject->setDefaultOrder(Config::NEWEST_SORTING_SORT_KEY);
+                $subject->setDefaultDirection('desc');
+            }
+            if ($catPath == '/best-sellers.html') {
+                $subject->setDefaultOrder(Config::BEST_SELLERS_SORTING_SORT_KEY);
+                $subject->setDefaultDirection('desc');
+            }
         }
 
         $collection->setOrder($subject->getCurrentOrder(), $subject->getCurrentDirection())->setOrder(Config::NEWEST_SORTING_SORT_KEY, 'desc');
